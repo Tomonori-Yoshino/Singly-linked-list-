a368274bae9ff4ca6da59d7916d74cdafb9d8927
Tomonori Yoshino <35744287+Tomonori-Yoshino@users.noreply.github.com>
2018-02-25 15:01:07 -0600

    Update MovieNode.cpp
    Change the overloaded < operator to manage the situation where the titles begins both in uppercase and lowercase letter.

--- MovieNode.cpp
+++ MovieNode.cpp
@@ -36,5 +36,25 @@ std::ostream& operator<< (std::ostream& fout, const MovieNode& node)
  */
 bool MovieNode::operator< ( const MovieNode& rhs ) const
 {
-    return movie.title < rhs.movie.title;
+    std::string upper_title1 = to_uppercase(movie.title);
+    std::string upper_title2 = to_uppercase(rhs.movie.title);
+    return upper_title1 < upper_title2;
+}
+
+
+
+/**
+ * @detailed    Convert the first letter of the title to uppercase.
+ *
+ * @param       s    movie's title to be converted to uppercase
+ *
+ * @return      string whose first letter has been converted to uppercase
+ */
+std::string to_uppercase(std::string s)
+{
+    char first_letter = s.at(0);            //extract the first letter
+    first_letter = toupper(first_letter);   //convert to uppercase
+    s.erase(s.begin());                     //erase the old first letter
+    s.insert(s.begin(), first_letter);      //insert the new first letter
+    return s;
 }
